1091a7675b0b4a62eab094f8def47d42d7136638
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-08-25 11:47:54 -0700

    Modified README Tag closure conversion fix from HTML

--- app/contacts/page.js
+++ app/contacts/page.js
@@ -1,5 +1,5 @@
 "use client"
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
 export default function Contacts() {
   const [nameString, setNameString] = useState();
