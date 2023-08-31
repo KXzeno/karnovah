c00d553d4496a54a3efb4ee71805de138bd138df
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-08-30 19:12:57 -0700

    Reset content for new slate
    Experimented with box dimensions

--- app/page.js
+++ app/page.js
@@ -3,10 +3,16 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="bg-slate-900 divide-slate-500 text-center mx-auto space-y-2 min-h-screen flex flex-col">
-        <h1 className="italic text-center hover-x">Kathekon</h1>
-        <p><em>Founder of <a href="https://karnovah.com/" rel="noreferrer" target="_blank" className="text-cyan-400">Karnovah</a></em></p>
-        <p className="text-[13px]/[17px] text-center italic">Aspiring Web Dev</p>
+    <div className="bg-slate-900 divide-slate-500 text-center mx-auto min-h-screen">
+      <h1 className="italic text-center hover-x">Kathekon</h1>
+      <p><em>Founder of <a href="https://karnovah.com/" rel="noreferrer" target="_blank" className="text-cyan-400">Karnovah</a></em></p>
+      <p className="text-[13px]/[17px] text-center italic mb-4">Aspiring Web Dev</p>
+      <div className="h-[200px] w-[200px] bg-green-200 mx-auto">
+        s 
       </div>
+      <div className="h-[200px] w-[200px] bg-red-200 mx-auto">
+        w 
+      </div>
+    </div>
   );
 }
