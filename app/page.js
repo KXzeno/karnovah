6e2119416dcdd579dacbdd36bee64311126dbf3a
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-01 23:06:46 -0700

    Transform container props and element classes

--- app/page.js
+++ app/page.js
@@ -4,21 +4,29 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <div className="div-main">
-      <Image
-        src="/img/cloud.png"
-        alt="cloud-png"
-        width="150"
-        height="48"
-      />
-      <h1 className="italic text-center hover-x">Kathekon</h1>
-      <p><em>Founder of <a href="https://karnovah.com/" rel="noreferrer" target="_blank" className="text-cyan-400">Karnovah</a></em></p>
-      <p className="text-[13px]/[17px] text-center italic mb-4">Aspiring Full-Stack <span className="text-yellow-300">Dev</span></p>
-      <Image
-        src="/img/cloud.png"
-        alt="cloud-png"
-        width="150"
-        height="48"
-      />
+      <div className="flex justify-center relative">
+        <Image
+          className="top-cloud"
+          src="/img/cloud.png"
+          alt="cloud-png"
+          width="150"
+          height="48"
+        />
+      </div>
+      <div>
+        <h1 className="italic text-center hover-x mt-20">Kathekon</h1>
+        <p><em>Founder of <a href="https://karnovah.com/" rel="noreferrer" target="_blank" className="text-cyan-400">Karnovah</a></em></p>
+        <p className="text-[13px]/[17px] text-center italic mb-4">Aspiring Full-Stack <span className="text-yellow-300">Dev</span></p>
+      </div>
+      <div className="flex justify-center relative">
+        <Image
+          className="bot-cloud"
+          src="/img/cloud.png"
+          alt="cloud-png"
+          width="150"
+          height="48"
+        />
+      </div>
       <div className="imageWrapper">
         <Image 
           src="/img/mountain.png"
