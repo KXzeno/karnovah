f47cfd58141dcc28b53b8a1d162274d888904391
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-06 09:52:52 -0700

    Modify content

--- app/page.js
+++ app/page.js
@@ -84,7 +84,7 @@ export default function Home() {
             <div className="md:relative top-14 left-2 md:text-right">
               <h3 className="my-4 md:relative right-[3.7rem]">Writing <span className="italic">and Reading</span></h3>
               <span className="font-mono md:relative right-[3.7rem]">"The most powerful person in the world is the story teller," </span>
-              <span className="font-urbanist md:relative right-[3.7rem]">Steve Jobs once said. I absorb literary worlds which offer models of realities that are conducive to a healthy outlook of the real; I write similar worlds with similar effects. </span>
+              <span className="font-urbanist md:relative right-[3.7rem]">a visionary once said. I absorb literary worlds which offer models of realities that are conducive to a healthy outlook of the real; I write similar worlds with similar effects. </span>
           </div>
             </div>
           </div>
