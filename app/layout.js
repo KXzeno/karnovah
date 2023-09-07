b690af749361502b9e3e0c09c1ad5f7eee38c3c1
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-07 00:45:33 -0700

    Import Head module and add OG meta tags

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import './globals.css'
 // import { Inter } from 'next/font/google'
 import { Urbanist, Inter, Cinzel } from 'next/font/google'
@@ -27,6 +28,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <meta property="og:url" content="https://karnovah.netlify.app" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={metadata.title} />
+        <meta property="og:description" content={metadata.description} />
+        <meta property="og:image" content={metadata.openGraph.images} />
+      </Head>
       <body className={`${cinzel.variable} ${inter.variable} ${urbanist.variable} font-sans`}>{children}</body>
     </html>
   )
