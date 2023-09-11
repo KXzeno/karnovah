fa459c18ff24e6ac2a3ef1412d86a6f6c6ff3f41
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-10 20:14:21 -0700

    Add div ctr for responsiveness

--- app/btstrp-tw-design/page.js
+++ app/btstrp-tw-design/page.js
@@ -41,97 +41,99 @@ export default function Home() {
       </div>
       */ }
 
-      <div className="container">
+      <div id="title">
 
-        <nav className="bg-violet-200">
-          <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-            <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                { /*  Mobile menu button */ }
-                <button 
-                  type="button" 
-                  className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" 
-                  aria-controls="mobile-menu"
-                  aria-expanded="isMobileMenuOpen"
-                  onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} // Toggle the mobile menu state
-                >
-                  <span className="absolute -inset-0.5"></span>
-                  <span className="sr-only">Open main menu</span>
-                  { /* 
+        <div className="container mx-auto">
+
+          <nav className="bg-violet-200">
+            <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+              <div className="relative flex h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  { /*  Mobile menu button */ }
+                  <button 
+                    type="button" 
+                    className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" 
+                    aria-controls="mobile-menu"
+                    aria-expanded="isMobileMenuOpen"
+                    onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)} // Toggle the mobile menu state
+                  >
+                    <span className="absolute -inset-0.5"></span>
+                    <span className="sr-only">Open main menu</span>
+                    { /* 
                   Icon when menu is closed.
 
                   Menu open: "hidden", Menu closed: "block"
                   */ }
-                  <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-                  </svg>
-                  { /* 
+                    <svg className="block h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                    { /* 
                   Icon when menu is open.
 
                   Menu open: "block", Menu closed: "hidden"
                   */ }
-                  <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                  </svg>
-                </button>
-              </div>
-              <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-                <div className="flex flex-shrink-0 items-center">
-                  <Image
-                    className="h-8 w-auto"
-                    src="/img/dog-img.jpg"
-                    width="16"
-                    height="16"
-                    alt="Your Company"
-                  />
+                    <svg className="hidden h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                  </button>
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
-                  <div className="flex space-x-4">
-                    { /*  Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"  */ }
-                    <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">News</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Updates</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                    <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex flex-shrink-0 items-center">
+                    <Image
+                      className="h-8 w-auto"
+                      src="/img/dog-img.jpg"
+                      width="16"
+                      height="16"
+                      alt="Your Company"
+                    />
+                  </div>
+                  <div className="hidden sm:ml-6 sm:block">
+                    <div className="flex space-x-4">
+                      { /*  Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"  */ }
+                      <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">News</a>
+                      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Updates</a>
+                      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                      <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                <button
-                  type="button"
-                  className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  onClick={toggleNotificationsDropdown} // Toggle the notifications dropdown state
-                >
-                  <span className="absolute -inset-1.5"></span>
-                  <span className="sr-only">View notifications</span>
-                  <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-                  </svg>
-                </button>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <button
+                    type="button"
+                    className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                    onClick={toggleNotificationsDropdown} // Toggle the notifications dropdown state
+                  >
+                    <span className="absolute -inset-1.5"></span>
+                    <span className="sr-only">View notifications</span>
+                    <svg className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+                    </svg>
+                  </button>
 
-                { /*  Profile dropdown  */ }
-                <div className="relative ml-3">
-                  <div>
-                    <button
-                      type="button"
-                      className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                      id="user-menu-button"
-                      aria-expanded={isProfileDropdownOpen ? 'true' : 'false'}
-                      aria-haspopup="true"
-                      onClick={toggleProfileDropdown}
-                    >
-                      <span className="absolute -inset-1.5"></span>
-                      <span className="sr-only">Open user menu</span>
-                      <Image 
-                        className="h-8 w-8 rounded-full" 
-                        src="/img/author.png" 
-                        width="16"
-                        height="16"
-                        alt=""
-                      />
-                    </button>
-                  </div>
+                  { /*  Profile dropdown  */ }
+                  <div className="relative ml-3">
+                    <div>
+                      <button
+                        type="button"
+                        className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+                        id="user-menu-button"
+                        aria-expanded={isProfileDropdownOpen ? 'true' : 'false'}
+                        aria-haspopup="true"
+                        onClick={toggleProfileDropdown}
+                      >
+                        <span className="absolute -inset-1.5"></span>
+                        <span className="sr-only">Open user menu</span>
+                        <Image 
+                          className="h-8 w-8 rounded-full" 
+                          src="/img/author.png" 
+                          width="16"
+                          height="16"
+                          alt=""
+                        />
+                      </button>
+                    </div>
 
-                  { /* 
+                    { /* 
                               Dropdown menu, show/hide based on menu state.
 
                               Entering: "transition ease-out duration-100"
@@ -141,54 +143,56 @@ export default function Home() {
                               From: "transform opacity-100 scale-100"
                               To: "transform opacity-0 scale-95"
                               */ }
-                  <div
-                    className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
+                    <div
+                      className={`absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none ${
     isProfileDropdownOpen ? 'block' : 'hidden'}`}
-                    role="menu"
-                    aria-orientation="vertical"
-                    aria-labelledby="user-menu-button"
-                    tabIndex="-1"
-                  >
-                    { /*  Active: "bg-gray-100", Not Active: ""  */ }
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
-                    <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                      role="menu"
+                      aria-orientation="vertical"
+                      aria-labelledby="user-menu-button"
+                      tabIndex="-1"
+                    >
+                      { /*  Active: "bg-gray-100", Not Active: ""  */ }
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">Your Profile</a>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-1">Settings</a>
+                      <a href="#" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-2">Sign out</a>
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
 
-          { /*  Mobile menu, show/hide based on menu state.  */ }
-          <div className={`sm:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu">
-            <div className="space-y-1 px-2 pb-3 pt-2">
-              { /*  Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"  */ }
-              <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">News</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Updates</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
-              <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+            { /*  Mobile menu, show/hide based on menu state.  */ }
+            <div className={`sm:hidden ${isMobileMenuOpen ? 'block' : 'hidden'}`} id="mobile-menu">
+              <div className="space-y-1 px-2 pb-3 pt-2">
+                { /*  Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"  */ }
+                <a href="#" className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">News</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Updates</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Projects</a>
+                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">About</a>
+              </div>
             </div>
-          </div>
-        </nav>
+          </nav>
 
-        { /* Title */ }
+          { /* Title */ }
 
-        <div className="lg:columns-2" id="title">
+          <div className="lg:columns-2" id="title">
 
-          <div className="">
-            <h1 className="font-cinzel text-[3rem] leading-normal font-extrabold">Need a professional web developer to rebuild your user experience?.</h1>
-            <button type="button">Download</button>
-            <button type="button">Download</button>
-          </div>
+            <div className="">
+              <h1 className="font-cinzel text-[3rem] leading-normal font-extrabold">Need a professional web developer to rebuild your user experience?.</h1>
+              <button type="button">Download</button>
+              <button type="button">Download</button>
+            </div>
 
-          <div className="">
-            <Image
-              className=""
-              src="/img/iphone6.png"
-              width="400"
-              height="807"
-              alt="iphone-mockup"
-            />
+            <div className="">
+              <Image
+                className=""
+                src="/img/iphone6.png"
+                width="400"
+                height="807"
+                alt="iphone-mockup"
+              />
+            </div>
+
           </div>
 
         </div>
@@ -208,7 +212,7 @@ export default function Home() {
 
       </div>
 
-      { /* testimonials */ }
+      { /* Testimonials */ }
 
       <div className="" id="testimonials">
 
