3f3051ac580f4c240b0288e173d87ae63cfea877
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-17 15:53:09 -0700

    Group local modules for efficient calls globally

---
+++ app/components/page.js
@@ -0,0 +1,6 @@
+export { default as Navbar } from './Navbar';
+export { default as Testimonials } from './Testimonials';
+export { default as Features } from './Features';
+export { default as Title } from './Title';
+export { default as Press } from './Press';
+
