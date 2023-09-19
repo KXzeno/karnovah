0b07b8bfdd687a8bdbfe55303589c9e98a8ab870
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-19 04:19:51 -0700

    Add custom cards in grid layout

--- app/btstrp-tw-design/page.js
+++ app/btstrp-tw-design/page.js
@@ -39,42 +39,58 @@ export default function Home() {
         { /* Testimonials */ }
 
         <Testimonials />
-        
+
         { /* Press */ }
 
         <Press />
 
         { /* Pricing */ }
 
-        <div className="container">
+        <div className="container mx-auto mt-4">
 
-          <div className="" id="pricing">
-
-            <h2>A Plan for Every Dog's Needs</h2>
-            <p>Simple and affordable price plans for you and your dog</p>
-
-
-            <h3>Chihuahua</h3>
-            <h2>Free</h2>
-            <p>5 Matches Per Day</p>
-            <p>10 Messages Per Day</p>
-            <p>Unlimited App Usage</p>
-            <button type="button">Sign Up</button>
-
-            <h3>Labrador</h3>
-            <h2>$49 / mo</h2>
-            <p>Unlimited Matches</p>
-            <p>Unlimited Messages</p>
-            <p>Unlimited App Usage</p>
-            <button type="button">Sign Up</button>
-
-            <h3>Mastiff</h3>
-            <h2>$99 / mo</h2>
-            <p>Priority Listing</p>
-            <p>Unlimited Matches</p>
-            <p>Unlimited Messages</p>
-            <p>Unlimited App Usage</p>
-            <button type="button">Sign Up</button>
+          <div className="grid grid-cols-3 max-md:grid-cols-1 gap-3 card text-center" id="pricing">
+            <div className="card-nested">
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="card-title">Recent Projects Showcase</h5>
+                </a>
+                <p className="card-content">My portfolio is a testament to my commitment to delivering exceptional web experiences. Take a look at my recent projects to see how I've transformed ideas into digital realities.</p>
+                <a href="#" className="card-more">
+                  See more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="card-nested">
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="card-title">Tech Insights and Blog</h5>
+                </a>
+                <p className="card-content">Stay informed and inspired with our tech-centric blog. Explore the latest trends, best practices, and insights in the world of web development and technology.</p>
+                <a href="#" className="card-more">
+                  Read more
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                  </svg>
+                </a>
+              </div>
+            </div>
+            <div className="card-nested">
+              <div className="p-5">
+                <a href="#">
+                  <h5 className="card-title">Get in Touch</h5>
+                </a>
+                <p className="card-content">Ready to elevate your online presence? Contact me today to discuss your project and discover how I can turn your ideas into a reality.</p>
+                <a href="#" className="relative card-more top-6">
+                  Contact me
+                  <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" strokeLinecap="round" stroke-linejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                  </svg>
+                </a>
+              </div>
+            </div>
 
           </div>
 
