b9c892ae026b3ef6944267c8f726a157d25057c8
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-19 18:16:10 -0700

    Refactor exporter
    Page.js converts to index.js as webpacker targets the index, not the
    page as page is to render and index is to index what is inside the
    folder. This allows for importing modules just by calling the folder.
    
    Improves performance as it targets the indexer.

---
+++ app/components/index.js
@@ -0,0 +1,15 @@
+import Carousel from './Carousel';
+import Features from './Features';
+import Navbar from './Navbar';
+import Press from './Press';
+import Testimonials from './Testimonials';
+import Title from './Title';
+
+export {
+  Carousel,
+  Features,
+  Navbar,
+  Press,
+  Testimonials,
+  Title,
+}
