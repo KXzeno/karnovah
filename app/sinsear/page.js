a8491259ef120bbe2b5fb8bbc7a30bffec628949
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-27 12:36:03 -0700

    Add content and styles

--- app/sinsear/page.js
+++ app/sinsear/page.js
@@ -1,13 +1,18 @@
-// Global imports
+/** Global Imports **/
 
 import React from 'react';
+import Image from 'next/image';
 
-// Local imports
+/** Local imports **/
+
+import './styles.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className="">
+      <div className="bg-gradient-to-t from-[#040D12] from-3% via-[#313866] via-90% to-[#352F44] to-95% min-h-screen">
      Filler text 
+      </div>
     </div>
   )
 }
