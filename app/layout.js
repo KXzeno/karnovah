b1ab2460cb7a99723b8efe195e440ca21c42055c
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-29 05:37:35 -0700

    Migrate to Metadata API
    Compiler gave warning for using 'next/head' in the 'app' directory

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,3 @@
-import Head from 'next/head'
 import './globals.css'
 // import { Inter } from 'next/font/google'
 import { Urbanist, Inter, Cinzel, Diphylleia, Roboto } from 'next/font/google'
@@ -33,6 +32,10 @@ export const metadata = {
   title: 'Karnovah',
   description: 'Personal Business & Blog Site Under Development',
   openGraph: {
+    url: 'https://karnovah.netlify.app',
+    type: 'website',
+    title: 'Karnovah',
+    description: 'Personal Business & Blog Site Under Development', 
     images: 'https://i.postimg.cc/bwm0ZQJb/kx.png'
   },
 }
@@ -40,13 +43,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
         <meta property="og:url" content="https://karnovah.netlify.app" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content={metadata.title} />
         <meta property="og:description" content={metadata.description} />
         <meta property="og:image" content={metadata.openGraph.images} />
-      </Head>
       <body className={`${diphylleia.variable} ${cinzel.variable} ${inter.variable} ${urbanist.variable} ${roboto.variable} font-sans`}>{children}
       </body>
     </html>
