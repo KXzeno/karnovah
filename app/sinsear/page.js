9f77b9cdfd7471e60fb1b902045b829097e0e4d0
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-09-28 21:33:27 -0700

    Add page content
    Experimenting with tailwind utilities, following a Tailwind guide on the
    possibilities of UI design

--- app/sinsear/page.js
+++ app/sinsear/page.js
@@ -36,8 +36,13 @@ export default function Home() {
         </nav>
       </div>
       <div className="bg-gradient-to-t from-[#040D12] from-3% via-[#313866] via-90% to-[#352F44] to-95% h-screen">
-        <div className="flex flex-col justify-center items-center -mt-64">
+        <div className="flex flex-col justify-center items-center pt-64">
           <p className="text-white text-5xl p-4 text-center">String 1</p>
+          <div className="flex flex-col sm:flex-row w-1/2 mb-4">
+            <button className="bg-white text-black rounded-full hover:text-white hover:bg-black mx-2 my-2 flex-grow border-4 border-white py-2 w-100 uppercase text-sm">String 2</button>
+            <button className="bg-white text-black rounded-full hover:text-white hover:bg-black mx-2 my-2 flex-grow border-4 border-white py-2 w-100 uppercase text-sm">String 3</button>
+          </div>
+          <a href="#" className="underline text-white text-lg tracking-wider">String 4</a>
         </div>
       </div>
     </div>
