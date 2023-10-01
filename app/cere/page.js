c3b8f7be1bbe2090d5f4f0b868a0f912d417be60
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-01 04:02:45 -0700

    Add contact form
    Marks end of intermodule Tailwind experimentation

--- app/cere/page.js
+++ app/cere/page.js
@@ -145,6 +145,34 @@ export default function Home() {
           </div>
         </div>
       </section>
+      <section id="contact" className="pt-20 pb-36 px-8 bg-gray-50">
+        <div className="max-w-6xl mx-auto">
+          <div className="text-center">
+            <h1 className="text-6xl font-bold text-gray-700">Contact</h1>
+            <p className="pt-2 text-xl">Get in touch</p>
+          </div>
+          <div className="mt-16 relative max-w-4xl mx-auto">
+            <div className="absolute z-10 inset-0 bg-gradient-to-r from-primary to-secondary shadow-lg transform -skew-y-6 sm:skew-y-0 sm:rotate-6 sm:rounded-lg">
+            </div>
+            <div className="relative z-20 bg-white rounded-md shadow-md p-12">
+              <form action="">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-x-8 gap-y-6">
+                  <input type="text" placeholder="Name" 
+                    className="border border-gray-200 outline-none px-4 py-2 rounded-md hover:border-gray-400 focus:border-gray-600 text-black placeholder:text-slate-400" />
+                  <input type="email" placeholder="E-mail" 
+                    className="border border-gray-200 outline-none px-4 py-2 rounded-md hover:border-gray-400 focus:border-gray-600 text-black" />
+                  <input type="text" placeholder="Subject" 
+                    className="border border-gray-200 outline-none px-4 py-2 rounded-md hover:border-gray-400 focus:border-gray-600 md:col-span-2 text-black" />
+                  <textarea 
+                    className="border border-gray-200 outline-none px-4 py-2 rounded-md hover:border-gray-400 focus:border-gray-600 md:col-span-2 text-black" 
+                    rows="5" placeholder="Message"></textarea>
+                </div>
+                <button className="inline-block w-auto mt-4 px-6 py-2 bg-gradient-to-r from-primary to-secondary rounded-md shadow-md text-white font-bold text-sm">Send Message</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
     </div>
   )
 }
