7f64dbe708c105de06740b731883fdd207a0aa91
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-03 04:09:11 -0700

    Add ID references from Navbar
    Ideally they should open up other pages. This is experimental

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -65,10 +65,10 @@ export default function Navbar() {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 { /*  Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"  */ }
-                <a href="#" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">News</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Updates</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
-                <a href="#" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
+                <a href="#news" className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page" id="news">News</a>
+                <a href="#updates" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Updates</a>
+                <a href="#projects" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Projects</a>
+                <a href="#about" className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About</a>
               </div>
             </div>
           </div>
