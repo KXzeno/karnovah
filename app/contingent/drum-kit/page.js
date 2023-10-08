cf127c7cd446725a3dd2fdce320290cdb4af7f05
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-08 13:01:04 -0700

    Convert vanilla HTML to RFC

---
+++ app/contingent/drum-kit/page.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Script from 'next/script';
+
+import './styles.css';
+
+export default function Home() {
+  return(
+    <>
+
+      <h1 id="title">Drum 🥁 Kit</h1>
+      <div class="set">
+        <button class="w drum">w</button>
+        <button class="a drum">a</button>
+        <button class="s drum">s</button>
+        <button class="d drum">d</button>
+        <button class="j drum">j</button>
+        <button class="k drum">k</button>
+        <button class="l drum">l</button>
+
+      </div>
+
+      <Script src="./script.js" />
+
+    </>
+  )
+}
