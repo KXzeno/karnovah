a795f801af29b76a532a4cb52d48f14a5e027c8f
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-08 13:00:02 -0700

    Experiment with audio tag

--- _vanillajs/drum_kit/index.js
+++ _vanillajs/drum_kit/index.js
@@ -15,6 +15,7 @@ let numDrumBtn = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numDrumBtn; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function() {
-    alert("Clicked");
+    let audio = new Audio("/sounds/tom-1.mp3");
+    audio.play();
   });
-}
+
