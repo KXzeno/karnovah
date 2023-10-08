bc59015ed1a91d01926dbc3f09bba72e208caa69
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-08 13:01:30 -0700

    Clone script in app routing

---
+++ app/contingent/drum-kit/script.js
@@ -0,0 +1,20 @@
+/* document.querySelector("button").addEventListener("click", handleClick);
+
+function handleClick() {
+  alert("Clicked");
+}
+*/
+
+/** Anonymous Function ver. **/
+
+// document.querySelector("button").addEventListener("click", function() {
+//  alert("Clicked");
+//  });
+
+let numDrumBtn = document.querySelectorAll(".drum").length;
+
+for (let i = 0; i < numDrumBtn; i++) {
+  document.querySelectorAll("button")[i].addEventListener("click", function() {
+    alert("Hello");
+  });
+
