748e4e2b531c79f15d88aed4bdbf535c65ea14e2
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-09 12:44:47 -0700

    Fix syntax error

--- _vanillajs/drum_kit/index.js
+++ _vanillajs/drum_kit/index.js
@@ -15,7 +15,7 @@ let numDrumBtn = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numDrumBtn; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function() {
-    let audio = new Audio("/sounds/tom-1.mp3");
+    let audio = new Audio("sounds/tom-1.mp3");
     audio.play();
   });
-
+}
