7d5876c6401e040af3ac047c1260c120fbd04370
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-11 13:01:30 -0700

    Implement switch statement for DOM handling

--- _vanillajs/drum_kit/index.js
+++ _vanillajs/drum_kit/index.js
@@ -1,3 +1,4 @@
+
 /* document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick() {
@@ -15,7 +16,48 @@ let numDrumBtn = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numDrumBtn; i++) {
   document.querySelectorAll("button")[i].addEventListener("click", function() {
-    let audio = new Audio("sounds/tom-1.mp3");
-    audio.play();
+
+    let buttonInnerHTML = this.innerHTML;
+
+    switch (buttonInnerHTML) {
+
+      case 'w':
+        let tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+      case 'a':
+        let tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+      case 's':
+        let tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+      case 'd':
+        let tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+      case 'j':
+        let snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+
+      case 'k':
+        let crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+      case 'l':
+        let kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+        break;
+
+      default: console.log(innerHTML);
+
+    }
   });
 }
