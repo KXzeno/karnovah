64b03dd6796ae14301dd018adc762845ab5a2b3d
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-12 17:53:32 -0700

    Implement keydown event listener
    Only keys 'w', 'a', and 's' are working. Resolve later

--- _vanillajs/drum_kit/index.js
+++ _vanillajs/drum_kit/index.js
@@ -12,6 +12,8 @@ function handleClick() {
 //  alert("Clicked");
 //  });
 
+// Detects Btn Press
+
 let numDrumBtn = document.querySelectorAll(".drum").length;
 
 for (let i = 0; i < numDrumBtn; i++) {
@@ -19,45 +21,60 @@ for (let i = 0; i < numDrumBtn; i++) {
 
     let buttonInnerHTML = this.innerHTML;
 
-    switch (buttonInnerHTML) {
+    soundEffect(buttonInnerHTML);
 
-      case 'w':
-        let tom1 = new Audio("sounds/tom-1.mp3");
-        tom1.play();
-        break;
-
-      case 'a':
-        let tom2 = new Audio("sounds/tom-2.mp3");
-        tom2.play();
-        break;
-
-      case 's':
-        let tom3 = new Audio("sounds/tom-3.mp3");
-        tom3.play();
-        break;
-
-      case 'd':
-        let tom4 = new Audio("sounds/tom-4.mp3");
-        tom4.play();
-        break;
-
-      case 'j':
-        let snare = new Audio("sounds/snare.mp3");
-        snare.play();
-        break;
-
-      case 'k':
-        let crash = new Audio("sounds/crash.mp3");
-        crash.play();
-        break;
-
-      case 'l':
-        let kick = new Audio("sounds/kick-bass.mp3");
-        kick.play();
-        break;
-
-      default: console.log(innerHTML);
-
-    }
   });
+}
+
+// Detects Keyboard Press
+
+document.addEventListener("keydown", function(event) {
+
+  soundEffect(event.key);
+
+});
+
+function soundEffect(key) {
+
+  switch (key) {
+
+    case 'w':
+      let tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+      break;
+
+    case 'a':
+      let tom2 = new Audio("sounds/tom-2.mp3");
+      tom2.play();
+      break;
+
+    case 's':
+      let tom3 = new Audio("sounds/tom-3.mp3");
+      tom3.play();
+      break;
+
+    case 'd':
+      let tom4 = new Audio("sounds/tom-4.mp3");
+      tom4.play();
+      break;
+
+    case 'j':
+      let snare = new Audio("sounds/snare.mp3");
+      snare.play();
+      break;
+
+    case 'k':
+      let crash = new Audio("sounds/crash.mp3");
+      crash.play();
+      break;
+
+    case 'l':
+      let kick = new Audio("sounds/kick-bass.mp3");
+      kick.play();
+      break;
+
+    default: console.log(innerHTML);
+
+  }
+
 }
