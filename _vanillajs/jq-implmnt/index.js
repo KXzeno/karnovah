77e38cc80650282857ec3ce95257ce30a4c0d1a7
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-17 00:51:50 -0700

    Manipulate attributes with jQ

--- _vanillajs/jq-implmnt/index.js
+++ _vanillajs/jq-implmnt/index.js
@@ -2,3 +2,6 @@ $("h1").css("color", "red");
 $("h1").text("Yo");
 $("h1").addClass("test-style").removeClass("test-style-x");
 $("button").html("<em>Btn</em>");
+console.log($("img").attr("src"));
+$("a").attr("href", "https://youtube.com");
+console.log($("h1").attr("class"));
