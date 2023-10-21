3acadcc0e0b53dc42957438b69733e3ed08597c0
Kasz <82054121+KXzeno@users.noreply.github.com>
2023-10-20 17:59:29 -0700

    Highlight color tags in DOM for animations

--- _vanillajs/simon/game.js
+++ _vanillajs/simon/game.js
@@ -5,4 +5,9 @@ function nextSequence() {
   let randomNum = Math.floor(Math.random() * 4);
   let randomClr = btnClrs[randomNum];
   gamePattern.push(randomClr);
+
+  $("#" + randomClr).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  let audio = new Audio("sounds/" + randomChosenColour + ".mp3");
+  audio.play();
 }
