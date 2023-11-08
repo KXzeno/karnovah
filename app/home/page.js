75c424719cbd57ae676718eb67034566ecbb1d42
git <82054121+KXzeno@users.noreply.github.com>
2023-11-08 05:02:38 -0800

    Add footer component

--- app/home/page.js
+++ app/home/page.js
@@ -5,12 +5,19 @@ import Image from 'next/image';
 
 /*Local Imports*/
 import './styles.css';
-import { MenuIcon, DefaultNav } from 'components';
+import { MenuIcon, DefaultNav, ChevronDownIcon } from 'components';
 
 export default function Home() {
   return(
     <>
       <DefaultNav />
+      <footer className="bottom-0 left-0 right-0 absolute">
+        <p className="text-center text-white leading-loose tracking-wider">String 5</p>
+        <div className="flex items-center text-center justify-center cursor-pointer">
+          <ChevronDownIcon className="fill-white" />
+        </div>
+      </footer>
+
     </>
   );
 }
