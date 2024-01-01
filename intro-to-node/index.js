942cdd7b50f18b76f55717032a5f49cd0bd013dd
git <82054121+KXzeno@users.noreply.github.com>
2024-01-01 08:42:16 -0800

    Create nested node project; test with npm

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,4 +1,13 @@
-import * as fs from 'node:fs';
+/* import * as fs from 'node:fs';
 
 fs.copyFileSync('file1.txt', 'bruh.txt');
 console.log('worked');
+*/ 
+
+// let superheroes = require("superheroes");
+import superheroes from 'superheroes';
+import supervillains from 'supervillains';
+
+let mySuperheroName = superheroes.random();
+console.log(mySuperheroName);
+console.log(supervillains.random());
