410ad8503304b4d7eac246a1e4c9ce116a7f8697
git <82054121+KXzeno@users.noreply.github.com>
2024-01-04 13:41:34 -0800

    Use HTTPS get to trigger a callback on API call

--- _other/Weather-Project/app.js
+++ _other/Weather-Project/app.js
@@ -4,7 +4,10 @@ import https from 'https';
 const app = express();
 
 app.get("/", function(req, res){
-  https.get
+  const url = "https://api.openweathermap.org/data/2.5/weather?lat=32.68&lon=-117.08&appid=68a923624c4e7817924b18ddd9c41ec6&units=imperial"
+  https.get(url, function(response){
+    console.log(response);
+  });
   res.send("Works");
 });
 
