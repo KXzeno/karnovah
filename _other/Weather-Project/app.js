d29acdbf120eab6e0e63119cdbd8ddee9512cd44
git <82054121+KXzeno@users.noreply.github.com>
2024-01-05 14:13:57 -0800

    Fix syntax error on property assignment

--- _other/Weather-Project/app.js
+++ _other/Weather-Project/app.js
@@ -13,7 +13,7 @@ app.get("/", function(req, res){
       console.log(weatherData);
       const obj = {
         name: "KX",
-        purpose "CONQUER",
+        purpose: "CONQUER",
       }
       console.log(JSON.stringify(obj));
     });
