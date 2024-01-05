76311e3f9076ad32d29024664125bd99a664bd4c
git <82054121+KXzeno@users.noreply.github.com>
2024-01-05 14:07:17 -0800

    Log HTTP status codes

--- _other/Weather-Project/app.js
+++ _other/Weather-Project/app.js
@@ -7,6 +7,7 @@ app.get("/", function(req, res){
   const url = "https://api.openweathermap.org/data/2.5/weather?lat=32.68&lon=-117.08&appid=68a923624c4e7817924b18ddd9c41ec6&units=imperial"
   https.get(url, function(response){
     console.log(response);
+    console.log(response.statusCode);
   });
   res.send("Works");
 });
