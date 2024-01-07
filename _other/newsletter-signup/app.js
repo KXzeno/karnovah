3b0a2ffa484e7475994efb3890477c78d15ca079
git <82054121+KXzeno@users.noreply.github.com>
2024-01-07 14:58:23 -0800

    Create payload conforming to newsletter API
    Mailchimp API requires data payload to be structured in a specific way

--- _other/newsletter-signup/app.js
+++ _other/newsletter-signup/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import request from 'request';
 import { dirname } from 'path';
+import 'dotenv/config';
+import https from 'https';
 
 const app = express();
 const dirName = dirname(import.meta.filename);
@@ -14,11 +16,38 @@ app.get("/", function(req, res) {
 });
 
 app.post("/", function(req, res) {
-  let firstName = req.body.fName;
-  let lastName = req.body.lName;
-  let email = req.body.email;
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
 
-  console.log(firstName, lastName, email);
+  const data = {
+    members: [
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstName,
+          LNAME: lastName,
+        }
+      }
+    ]
+  };
+
+  let jsonData = JSON.stringify(data);
+  const url = `https://${process.env.server}.api.mailchimp.com/3.0/lists/${process.env.audience_id}`;
+  console.log(url);
+  const options = {
+    method: "POST",
+    auth: `kx1:${process.env.apiKey}`,
+  }
+
+  const request = https.request(url, options, function(response) {
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+  request.write(jsonData);
+  request.end();
 });
 
 app.listen(3000, function() {
