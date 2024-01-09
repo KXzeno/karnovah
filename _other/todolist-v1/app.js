5066fe42057e2e244e9540692f560a4f3bbf60e0
git <82054121+KXzeno@users.noreply.github.com>
2024-01-09 00:01:01 -0800

    Refactor code using ternary and built-ins

--- _other/todolist-v1/app.js
+++ _other/todolist-v1/app.js
@@ -4,17 +4,26 @@ import { dirname } from 'path';
 
 const app = express();
 const dirName = dirname(import.meta.filename);
+app.set('view engine', 'ejs');
 
 app.get("/", function(req, res) {
   let today = new Date();
   let day = "";
+  /*
   if (today.getDay() === 6 || today.getDay() === 0 ) {
     day = "Weekend";
     res.write("Work optional");
   } else {
     day = "Weekend";
   }
-  res.render("list", {kindOfDay: day});
+  */
+
+  let dayType = (today.getDay() % 6 == 0) ? "weekend" : "weekday";
+  const options = { weekday: "long" };
+  day = new Intl.DateTimeFormat("en-US", options).format(today);
+  res.render("list", {
+    day: day
+  });
 });
 
 app.listen(3000, function() { 
