42074b5fa9418bb0b61aae9cece9e58d25f3be4f
git <82054121+KXzeno@users.noreply.github.com>
2024-01-09 03:45:51 -0800

    Access payload and convert data to frontend

--- _other/todolist-v1/app.js
+++ _other/todolist-v1/app.js
@@ -5,6 +5,8 @@ import { dirname } from 'path';
 const app = express();
 const dirName = dirname(import.meta.filename);
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+let items = [];
 
 app.get("/", function(req, res) {
   let today = new Date();
@@ -24,7 +26,14 @@ app.get("/", function(req, res) {
   res.render("list", {
     day: day,
     weekEnd: weekEnd,
+    newListItem: items,
   });
+});
+
+app.post("/", function(req, res) {
+  let item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function() { 
