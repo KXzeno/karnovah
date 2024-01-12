af28d4c7ff0a31ab465e5639db6ea1d49f2bcc14
git <82054121+KXzeno@users.noreply.github.com>
2024-01-11 16:49:40 -0800

    Refactor using ECMAScript Module standard

--- _other/todolist-v1/date.js
+++ _other/todolist-v1/date.js
@@ -1,14 +1,13 @@
-//jshint esversion:6
-
-module.exports = getDate;
-
 function getDate() {
   let today = new Date();
-  let day = "";
-
   let weekEnd = (!(today.getDay() ^ 6) || !(today.getDay() ^ 0)) ? true : false;
-  const options = { weekday: "long" };
-  day = new Intl.DateTimeFormat("en-US", options).format(today);
+  const options = { 
+    weekday: "long" 
+  };
 
-  return day;
+  let day = new Intl.DateTimeFormat("en-US", options).format(today);
+
+  return { day, weekEnd };
 }
+
+export { getDate }
