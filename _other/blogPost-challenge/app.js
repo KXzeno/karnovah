ea0a046b7ed444337b6bda25302d22b067b38557
git <82054121+KXzeno@users.noreply.github.com>
2024-01-12 19:52:04 -0800

    Use passthrough object for home starting content
    Challenge 2 complete

--- _other/blogPost-challenge/app.js
+++ _other/blogPost-challenge/app.js
@@ -19,7 +19,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res) {
-  res.render("home.ejs");
+  res.render("home", {
+    homeStartingContent: homeStartingContent
+  });
 });
 
 
