2f4a3bb2c03b761b49c1803f82fd09d99e772459
git <82054121+KXzeno@users.noreply.github.com>
2024-01-17 01:33:10 -0800

    Create schema and use to create data

--- _other/database-sample/app.js
+++ _other/database-sample/app.js
@@ -2,25 +2,16 @@ import mongoose from 'mongoose';
 
 mongoose.connect("mongodb://localhost:27017/kaeon");
 
-const insertDocuments = function(db, callback) {
-  const collection = db.collection('one');
-  collection.insertMany([
-    {
-      name: "Kx",
-      num: 7
-    },
-    {
-      name: "Kaeon",
-      num: 3
-    }
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-};
+const schema = new mongoose.Schema({
+  name: String,
+  num: Number,
+});
 
-const findDocuments = function(db, callback) {
-}
+const Entity = mongoose.model("Entity", schema);
+
+const entity = new Entity({
+  name: "Kaeon",
+  num: 7
+});
+
+entity.save();
