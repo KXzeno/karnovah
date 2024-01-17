ce9d85a72d9e976ee7218257813604a219e058ac
git <82054121+KXzeno@users.noreply.github.com>
2024-01-17 01:51:51 -0800

    Create new schemas and create data sets

--- _other/database-sample/app.js
+++ _other/database-sample/app.js
@@ -11,7 +11,35 @@ const Entity = mongoose.model("Entity", schema);
 
 const entity = new Entity({
   name: "Kaeon",
-  num: 7
+  num: 7,
 });
 
 entity.save();
+
+const anotherSchema = new mongoose.Schema({
+  occupation: String,
+  age: Number,
+})
+
+const Person = mongoose.model("Person", anotherSchema);
+
+const person = new Person({
+  occupation: "Developer",
+  age: 7,
+})
+
+person.save();
+
+const cogdiss = new Entity({
+  name: "Cognitive Dissonance",
+  num: 14,
+});
+
+const fortress = new Entity({
+  name: "vertical",
+  num: 25,
+});
+
+Entity.insertMany([cogdiss, fortress]);
+
+
