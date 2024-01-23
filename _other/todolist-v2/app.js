1a980142fbf3cf0ea5ea62091c0d29a40ffe7d7c
git <82054121+KXzeno@users.noreply.github.com>
2024-01-23 10:30:39 -0800

    Remove async wrapping of program

--- _other/todolist-v2/app.js
+++ _other/todolist-v2/app.js
@@ -11,91 +11,88 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-async function main() {
-  mongoose.connect("mongodb://localhost:27017/todolistDB")
+mongoose.connect("mongodb://localhost:27017/todolistDB")
 
-  const itemsSchema = new mongoose.Schema({
-    name : String,
-  });
+const itemsSchema = new mongoose.Schema({
+  name : String,
+});
 
-  const Item = mongoose.model("Item", itemsSchema);
+const Item = mongoose.model("Item", itemsSchema);
 
-  const item1 = new Item({
-    name: "One",
-  });
+const item1 = new Item({
+  name: "One",
+});
 
-  const item2 = new Item({
-    name: "Two",
-  });
+const item2 = new Item({
+  name: "Two",
+});
 
-  const item3 = new Item({
-    name: "Three",
-  });
+const item3 = new Item({
+  name: "Three",
+});
 
-  const defaultItems = [item1, item2, item3];
+const defaultItems = [item1, item2, item3];
 
-  app.get("/", async function(req, res) {
-    // const { day, weekEnd } = getDate();
-    try {
-      const foundItems = await Item.find({});
-      foundItems.length === 0 
-        ? (() => {
-          try {
-            Item.insertMany(defaultItems);
-            console.log("Data change successful");
-          } catch (err) {
-            console.log ("Data change failed");
-          }
-          res.redirect("/");
-        })()
-        : console.log("Data did not update");
+app.get("/", async function(req, res) {
+  // const { day, weekEnd } = getDate();
+  try {
+    const foundItems = await Item.find({});
+    foundItems.length === 0 
+      ? (() => {
+        try {
+          Item.insertMany(defaultItems);
+          console.log("Data change successful");
+        } catch (err) {
+          console.log ("Data change failed");
+        }
+        res.redirect("/");
+      })()
+      : console.log("Data did not update");
 
-      console.log(foundItems);
-      res.render("list", {
-        listTitle: "Today",
-        // weekEnd: weekEnd,
-        newListItems: foundItems,
-      });
-      console.log(newListItems);
-    }
-    catch (err) {
-      console.log("Failed to render items");
-    }
-  });
-
-  app.post("/", function(req, res) {
-    const item = req.body.newItem;
-
-    console.log(req.body.list);
-    if (req.body.list === "Work List") {
-      workItems.push(item);
-      res.redirect("/work");
-    } else {
-      items.push(item);
-      res.redirect("/");
-    }
-
-  });
-
-  app.get("/work", function(req, res) {
+    console.log(foundItems);
     res.render("list", {
-      listTitle: "Work List",
-      newListItems: workItems,
+      listTitle: "Today",
+      // weekEnd: weekEnd,
+      newListItems: foundItems,
     });
-  });
+    console.log(newListItems);
+  }
+  catch (err) {
+    console.log("Failed to render items");
+  }
+});
 
-  app.post("/work", function(req, res) {
-    const item = req.body.newItem;
+app.post("/", function(req, res) {
+  const item = req.body.newItem;
+
+  console.log(req.body.list);
+  if (req.body.list === "Work List") {
     workItems.push(item);
     res.redirect("/work");
-  });
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
 
-  app.get("/about", function(req, res) {
-    res.render("about");
-  });
+});
 
-  app.listen(3000, function() { 
-    console.log("Port 3000 initialized...");
+app.get("/work", function(req, res) {
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems,
   });
-}
-main();
+});
+
+app.post("/work", function(req, res) {
+  const item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function(req, res) {
+  res.render("about");
+});
+
+app.listen(3000, function() { 
+  console.log("Port 3000 initialized...");
+});
