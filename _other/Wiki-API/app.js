0f1716cb48933c4d9f822e9f9bf6f68a11d9b9a0
git <82054121+KXzeno@users.noreply.github.com>
2024-01-26 13:13:03 -0800

    Add starting code for RESTful API project

---
+++ _other/Wiki-API/app.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import 'dotenv/config';
+import mongoose from 'mongoose';
+import ejs from 'ejs';
+import bodyParser from 'body-parser';
+
+const { Schema } = mongoose;
+const [DB_UID, DB_PASS] = [process.env.DB_AD_U, process.env.DB_AD_P];
+const uri = `mongodb+srv://${DB_UID}:${DB_PASS}@kharner.frb2ipg.mongodb.net/`;
+async function main() {
+  await mongoose.connect(uri, {
+    dbName: 'wikiDB',
+  });
+  const app = express();
+
+  app.set('view engine', 'ejs');
+  app.use(bodyParser.urlencoded({
+    extended: true,
+  }));
+  app.use(express.static("public"));
+
+
+  app.listen(3000, () => {
+    console.log("Server started...");
+  });
+  
+}
+main();
