22ee2f99e370d5641bb85731bb8fc20a694b361b
git <82054121+KXzeno@users.noreply.github.com>
2024-01-26 06:11:05 -0800

    Use Model query methods to server render posts

--- _other/bPC-2.0/app.js
+++ _other/bPC-2.0/app.js
@@ -8,10 +8,12 @@ import 'dotenv/config';
 
 /* Local Imports */
 
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const [ homeStartingContent, aboutContent, contactContent ] = 
+  [ "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.",
+    "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.",
+    "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero." ]
 
+const staticPosts = [ homeStartingContent, aboutContent, contactContent];
 const app = express();
 
 const DB_ID = process.env.DB_AD_U;
@@ -34,8 +36,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const posts = [];
-
 function truncate(str, maxlength) {
   return (str.length > maxlength) ?
     str.slice(0, maxlength - 1) + '…' : str;
@@ -45,11 +45,19 @@ app.get("/home", (req, res) => {
   res.redirect("/");
 });
 
-app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    posts: posts,
-  });
+app.get("/", async (req, res) => {
+//  const postData = Blog.find({});
+//  console.log(postData);
+  await Blog.find({}).exec()
+    .then(result => {
+//  console.log(typeof result);
+      res.render("home", {
+        homeStartingContent: homeStartingContent,
+        posts: result,
+      });
+    }).catch(err => {
+      console.error("Error in loading documents")
+    });
 });
 
 
@@ -70,24 +78,29 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
-app.post("/compose", (req, res) => {
-  let post = {
+app.post("/compose", async (req, res) => {
+  const post = new Blog({
     title: req.body.title,
     content: truncate(req.body.content, 100),
-  };
+  });
+  await post.save();
+
   post ? posts.push(post) : !!post;
   res.redirect("/");
 });
 
-app.get("/posts/:post", (req, res) => {
+app.get("/posts/:post", async (req, res) => {
   //let {...decon} = posts;
-  const slug = posts.find(({ title }) => title === req.params.post);
-
-  res.render("post", {
-    title: slug.title,
-    content: slug.content,
-  });
-
+  let slug = posts.find(({ title }) => title === req.params.post);
+  await Blog.findOne({ title: req.params.post }).exec()
+    .then(result => {
+      res.render("post", {
+        title: slug.title,
+        content: slug.content,
+      });
+    }).catch(err => {
+      console.error("Page not found.");
+    });
 });
 
 
