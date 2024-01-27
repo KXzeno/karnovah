f90a16f8ab32793dc59890164e04c361594f3d23
git <82054121+KXzeno@users.noreply.github.com>
2024-01-27 11:55:08 -0800

    feat: add get requests for three routes

--- _other/Secrets/app.js
+++ _other/Secrets/app.js
@@ -9,10 +9,20 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+app.get("/", (req, res) => {
+  res.render("home");
+});
 
+app.get("/login", (req, res) => {
+  res.render("login");
+});
 
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 
 app.listen(3000, () => {
   console.log("Server running...");
-})
+});
+
