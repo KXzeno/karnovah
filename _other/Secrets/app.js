26391747f93e509c0d59d01f80d99200d6217c8b
git <82054121+KXzeno@users.noreply.github.com>
2024-01-30 18:25:19 -0800

    Set authentication and reconfigure serialization

--- _other/Secrets/app.js
+++ _other/Secrets/app.js
@@ -48,8 +48,14 @@ userSchema.plugin(findOrCreate);
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser()); 
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+passport.deserializeUser((id, done) => {
+    User.findById(id, (err, user) => {
+        done(err, user);
+    });
+}); 
 
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
@@ -68,9 +74,13 @@ app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/auth/google", (req, res) => {
-    passport.authenticate("google", { scope: ["profile"] });
-});
+app.get("/auth/google", 
+    passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get("/auth/google/secrets",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    (req, res) => res.redirect("/secrets")); // Successful auth, redirect home.
 
 app.get("/login", (req, res) => {
     res.render("login");
