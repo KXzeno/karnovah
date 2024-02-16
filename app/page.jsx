a8835a803f79c1d6fd56a52ea65298dfc8053e3b
git <82054121+KXzeno@users.noreply.github.com>
2024-02-15 22:54:21 -0800

    style: add responsive elipse state

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,26 @@
+"use client";
 import React from 'react';
 import Image from 'next/image';
 
 export default function Home() {
+  let [notice, setNotice] = React.useState("Ongoing Site Reconstruction");
+
+  // Automate elipse animation
+  React.useEffect(() => {
+    const timer = setTimeout(() => {
+      setNotice(prevNotice => prevNotice === "Ongoing Site Reconstruction..."
+        ? "Ongoing Site Reconstruction"
+        : `${prevNotice}.`);
+    }, 500);
+    return () => clearTimeout(timer);
+  }, [notice]);
+
   return(
     <>
       <div className="container min-w-full min-h-screen">
         <div className="flex flex-auto bg-black min-h-screen">
           <div className="place-self-center mx-auto animate-bounce hover:animate-spin">
-            <p className="z-10 font-sono text-[2rem]">Ongoing Site Reconstruction...</p>
+            <p className="z-10 font-sono text-[2rem] max-sm:text-[1.1rem]">{notice}</p>
           </div>
         </div>
       </div>
