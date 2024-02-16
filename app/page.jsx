f2dd598eb5a5f92ac279edd4a31db1687a64d25f
git <82054121+KXzeno@users.noreply.github.com>
2024-02-15 17:57:21 -0800

    Empty landing page for project restructuring

--- app/page.jsx
+++ app/page.jsx
@@ -5,12 +5,28 @@ export default function Home() {
   return(
     <>
       <div className="container min-w-full min-h-screen">
-        <div className="flex flex-auto">
-          <div className="mx-auto">
-            Test
+        <div className="flex flex-auto bg-black min-h-screen">
+          <div className="place-self-center mx-auto animate-bounce hover:animate-spin">
+            <p className="z-10 font-sono text-[2rem]">Ongoing Site Reconstruction...</p>
           </div>
         </div>
       </div>
     </>
   );
 }
+
+{ /*
+      <div className="container min-w-full min-h-screen bg-slate-900">
+        <div className="flex flex-auto bg-black">
+          <div className="mx-auto">
+            <p>Yes</p>
+          </div>
+        </div>
+        <div className="relative h-32 w-32 bg-black mt-8 mx-auto">
+          <div className="absolute text-center inset-0"> 
+            <p>Yes</p>
+            <p>Yes</p>
+          </div>
+        </div>
+      </div>
+      */ }{/* inset-0 -> |t|b|l|r = 0 */}
