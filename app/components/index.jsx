1f53dab43976fdf605915c62239b71619686b292
git <82054121+KXzeno@users.noreply.github.com>
2024-03-03 14:43:19 -0800

    Remove deprecated components

--- app/components/index.jsx
+++ app/components/index.jsx
@@ -1,28 +1,28 @@
-import Carousel from './Carousel';
-import Features from './Features';
-import Navbar from './Navbar';
-import Press from './Press';
-import Testimonials from './Testimonials';
-import Title from './Title';
-import Footer from './Footer';
-import FooterOne from './FooterOne';
-import More from './More-Details';
-import { MenuIcon, DiscordIcon, ChevronDownIcon, } from './Icons';
-import DefaultNav from './DefaultNav';
+// import Carousel from './Carousel';
+// import Features from './Features';
+// import Navbar from './Navbar';
+// import Press from './Press';
+// import Testimonials from './Testimonials';
+// import Title from './Title';
+// import Footer from './Footer';
+// import FooterOne from './FooterOne';
+// import More from './More-Details';
+// import { MenuIcon, DiscordIcon, ChevronDownIcon, } from './Icons';
+// import DefaultNav from './DefaultNav';
 
-export {
-  Carousel,
-  Features,
-  Navbar,
-  Press,
-  Testimonials,
-  Title,
-  Footer,
-  FooterOne,
-  More,
-  MenuIcon, DiscordIcon, ChevronDownIcon,
-  DefaultNav,
-}
+// export {
+//   Carousel,
+//   Features,
+//   Navbar,
+//   Press,
+//   Testimonials,
+//   Title,
+//   Footer,
+//   FooterOne,
+//   More,
+//   MenuIcon, DiscordIcon, ChevronDownIcon,
+//   DefaultNav,
+// }
 
 // Named exports = modules containing a library or pack of functions 
 
