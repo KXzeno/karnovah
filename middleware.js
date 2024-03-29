584ff505938eabb74987dd2829c73a7904d8651d
git <82054121+KXzeno@users.noreply.github.com>
2024-03-29 03:06:36 -0700

    fix: handle nested routes on two responses

--- middleware.js
+++ middleware.js
@@ -1,13 +1,24 @@
 import { NextResponse } from 'next/server'
- 
-// This function can be marked `async` if using `await` inside
-export function middleware(request) {
-  let path = request.nextUrl.pathname + request.nextUrl.search;
 
-  return NextResponse.redirect(new URL(`https://blog.karnovah.com/${path}`, request.url));
+export function middleware(request) {
+  // Gets baseURL leaf and excludes '/'
+  let pathname = request.nextUrl.pathname.substring(1, request.nextUrl.pathname.length);
+
+  // Splits nested routes into names
+  let splitPaths = pathname.split('/');
+
+  // Outputs nested routes to the new URL excluding '/blog' and '/blogs'
+  if (splitPaths[0].includes('blogs')) {
+    splitPaths.shift();
+    pathname = splitPaths.join('/');
+  } else if (splitPaths[0].includes('blog')) {
+    splitPaths.shift();
+    pathname = splitPaths.join('/');
+  }
+
+  return NextResponse.redirect(new URL(`https://blog.karnovah.com/${pathname}`, request.url));
 }
- 
-// See "Matching Paths" below to learn more
+
 export const config = {
   matcher: ['/blog/:path*', '/blogs/:path*',],
 }
