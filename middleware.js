6389d9aa76c21b4fa725c809d1f4591f831bfd07
git <82054121+KXzeno@users.noreply.github.com>
2024-03-29 02:03:05 -0700

    feat: handle slugged responses on url reroute

--- middleware.js
+++ middleware.js
@@ -2,7 +2,9 @@ import { NextResponse } from 'next/server'
  
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  return NextResponse.redirect(new URL('https://blog.karnovah.com', request.url));
+  let path = request.nextUrl.pathname + request.nextUrl.search;
+
+  return NextResponse.redirect(new URL(`https://blog.karnovah.com/${path}`, request.url));
 }
  
 // See "Matching Paths" below to learn more
