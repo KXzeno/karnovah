b5f04077f6f867cc12027a35c24e19685d9af774
git <82054121+KXzeno@users.noreply.github.com>
2024-07-04 03:50:20 -0700

    feat: dynamically map xml data to RSS viewer

--- app/curiograph/page.tsx
+++ app/curiograph/page.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {ReactNode} from 'react';
 import Link from 'next/link';
 import Parser from 'rss-parser';
 import RSSParser from 'rss-parser';
@@ -21,18 +21,51 @@ let parse: (url: string) => Promise<object>  = async url => {
 }
 
 export default async function Curiograph() {
-  let feeds: Array<object> = [await parse(FEED_URLS[0]), await parse(FEED_URLS[1]), await parse(FEED_URLS[2])];
+  let feeds: Array<object> = [];
+
+  for (let i = 0; i < FEED_URLS.length; i++) {
+    let post = await parse(FEED_URLS[i]);
+    feeds.push(post);
+  }
+
+  function compilePosts(arr: Array<object>): Array<object> {
+    let postStack: Array<object> = [];
+    let SOURCE_COUNT = Object.keys(arr).length as const;
+    for (let i = 0; i < SOURCE_COUNT; i++) {
+      let source = arr[i];
+      let posts = Object.values(source);
+      for (let j = 0; j < posts.length; j++) {
+        postStack.push(source[j]);
+      }
+      postStack.push({ terminal: true });
+    }
+    return postStack;
+  }
+
+  function displayPosts(arr: Array<object>, refragment: boolean = false): ReactNode {
+    let compiledFeed: Array<object>;
+    let fragment: number = 1;
+    compiledFeed = (refragment) ? arr : compilePosts(arr);
+    return Object.values(compiledFeed).map(({ title, link, description, pubDate, guid, terminal }, index) => {
+      compiledFeed.shift();
+      if (terminal && terminal === true) {
+        fragment++;
+        displayPosts(compiledFeed, true);
+        return;
+      }
+
+      return (
+        <div className={`grid grid-cols-subgrid col-start-${fragment % 2 + 1}`} key={title}>
+          {<Link href={link}>{title}</Link>}
+        </div>
+      )
+    }) as ReactNode
+  }
 
   return (
     <div className='relative min-h-screen w-screen'>
-      <div className='flex-row text-center space-y-4'>
-        <p>Begins here</p>
-        {Object.values(feeds[2]).map(({ title, link, description, pubDate, guid }) => {
-            return (
-              <div key={pubDate}>
-                <Link href={link} >{title}</Link>
-              </div>);
-          })}
+      <div className='grid grid-cols-2 text-center'>
+        {displayPosts(feeds)}
       </div>
     </div>
   );
