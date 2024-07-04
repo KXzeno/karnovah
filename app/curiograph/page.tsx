88bdcef66955a80be1689b94726c813cc2fafb3f
git <82054121+KXzeno@users.noreply.github.com>
2024-07-03 18:00:31 -0700

    feat: add Art of Manliness RSS

--- app/curiograph/page.tsx
+++ app/curiograph/page.tsx
@@ -5,8 +5,11 @@ import RSSParser from 'rss-parser';
 
 // TODO: Reduce provider workload on updating RSS lists
 
-let FEED_URLS: Array<string> = ['https://www.nasa.gov/feed/',
-  'https://www.reddit.com/r/logHorizon.rss'] as const;
+let FEED_URLS: Array<string> = [
+  'https://www.nasa.gov/feed/',
+  'https://www.reddit.com/r/logHorizon.rss',
+  'https://www.artofmanliness.com/feed/'
+] as const;
 
 let parser: Parser = new RSSParser();
 
@@ -18,13 +21,13 @@ let parse: (url: string) => Promise<object>  = async url => {
 }
 
 export default async function Curiograph() {
-  let feeds: Array<object> = [await parse(FEED_URLS[0]), await parse(FEED_URLS[1])];
+  let feeds: Array<object> = [await parse(FEED_URLS[0]), await parse(FEED_URLS[1]), await parse(FEED_URLS[2])];
 
   return (
     <div className='relative min-h-screen w-screen'>
       <div className='flex-row text-center space-y-4'>
         <p>Begins here</p>
-        {Object.values(feeds[0]).map(({ title, link, description, pubDate, guid }) => {
+        {Object.values(feeds[2]).map(({ title, link, description, pubDate, guid }) => {
             return (
               <div key={pubDate}>
                 <Link href={link} >{title}</Link>
