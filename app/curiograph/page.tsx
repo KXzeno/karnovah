d8df40480766d3689668f6a75319e461e3dc119b
git <82054121+KXzeno@users.noreply.github.com>
2024-07-07 00:32:38 -0700

    feat: introduce and style feed content

--- app/curiograph/page.tsx
+++ app/curiograph/page.tsx
@@ -3,15 +3,12 @@ import Link from 'next/link';
 import Parser from 'rss-parser';
 import RSSParser from 'rss-parser';
 
-// TODO: Reduce provider workload on updating RSS lists
-// TODO: Demarcate RFCS, use this as client RFC and merge
-// with server RFC
-
 let FEED_URLS: Array<string> = [
   'https://www.nasa.gov/feed/',
   'https://www.reddit.com/r/logHorizon.rss',
   'https://www.artofmanliness.com/feed/',
   'https://www.youtube.com/feeds/videos.xml?channel_id=UC7Gow-kNHq21oejSIDg9PAg',
+  'https://www.youtube.com/feeds/videos.xml?channel_id=UC4YaOt1yT-ZeyB0OmxHgolA',
 ] as const;
 
 let parser: Parser = new RSSParser();
@@ -48,17 +45,20 @@ export default async function Curiograph() {
   let FEED_COUNT: number = feeds.length;
 
   function renderPosts(data: PostData, index: number) {
-
+    // console.log(data);
     return (
       <>
-         <div key={`index + ${data.title}`} className={`col-start-${(FEED_COUNT + 1 - feeds.length) % 2 === 0 ? 1 : 2} col-span-1 text-center ${index === 0 ? 'text-red-700' : 'text-inherit'}`}>
-           <Link 
-             href={`${data.link}`}
-             className='text-inherit no-underline hover:underline'
-           >
-             {data.title}
-           </Link>
-         </div>
+        <div className='mb-2'>
+          <Link 
+            href={`${data.link}`}
+            className='text-inherit no-underline hover:underline text-sm'
+          >
+            {data.title}
+          </Link>
+        </div>
+        <p className='text-xs'>
+          {data.description || data.contentSnippet}
+        </p>
       </>
     )
   }
@@ -69,22 +69,24 @@ export default async function Curiograph() {
 
     for (let i = 0; i < Object.keys(feeds[0]).length; i++) {
       // Capacity limiter
-      // if (i >= 10) break;
+      if (i >= 2) break;
       posts.push(feeds[0][i]);
     }
 
     feeds.shift();
 
-    //TODO: Recursion inside method for nested items + subgrid
-
     return (
       <>
-        <div className=/*'grid grid-cols-subgrid h-max'*/'inline-flex flex-col'>
+        <div className='grid grid-cols-subgrid place-self-center'>
           {posts.map((data: PostData, index: number) => {
             return (
-              <>
-              {renderPosts(data, index)}
-              </>
+              <div 
+                key={`${index}-${data.title}`}
+                className={`col-start-${(FEED_COUNT + 1 - feeds.length) % 2 === 0 ? 1 : 2} 
+                  col-span-1 text-left ${index === 0 ? 'text-inherit' : 'text-inherit'} my-auto`}
+              >
+                {renderPosts(data, index)}
+              </div>
             )
           })}
         </div>
@@ -100,8 +102,8 @@ export default async function Curiograph() {
    */
 
   return (
-    <div className='relative min-h-screen w-screen'>
-      <div className='grid grid-cols-2'>
+    <div className='relative min-h-screen w-screen mx-auto'>
+      <div className='grid grid-cols-1 gap-1 divide-y divide-neutral-500'>
         {displayPosts()}
       </div>
     </div>
