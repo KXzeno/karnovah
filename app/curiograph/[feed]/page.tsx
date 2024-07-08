3a11f48f70164c81d74be0f7c6811a0c64865b59
git <82054121+KXzeno@users.noreply.github.com>
2024-07-08 09:10:13 -0700

    FEAT: add expansive feed on dynamic routing

--- app/curiograph/[feed]/page.tsx
+++ app/curiograph/[feed]/page.tsx
@@ -1,28 +1,10 @@
 import React from 'react';
-import FEED_SRCS from '@P/feeds.json';
+import Feed from '@F/Feed';
 
-export default function Feed({ params }: { params: { feed: string }}): React.ReactNode {
-
-  function verifyQuery(feed: string): Array<string | boolean> | false {
-    for (let i = 0; i < Object.keys(FEED_SRCS).length; i++) {
-      if (FEED_SRCS[i] && feed === FEED_SRCS[i].substring(FEED_SRCS[i].indexOf(',') + 1)) return [feed, true];
-      if (i === Object.keys(FEED_SRCS).length - 1) return false;
-    }
-  }
-
-  let QUERY_CHECK = verifyQuery(params.feed);
-
+export default function ExpandedFeed({ params }: { params: { feed: string }}): React.ReactNode {
   return (
     <>
-      {QUERY_CHECK ? 
-        <>
-          <h1>ID: {params.feed}</h1>
-        </>
-        :
-        <>
-          y
-        </>
-      }
+      <Feed filter={params.feed}/>
     </>
   );
 }
