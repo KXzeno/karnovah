9e7b741f03850fc15be14956e103b20cd4b4a5e1
git <82054121+KXzeno@users.noreply.github.com>
2024-07-12 07:52:44 -0700

    Add secret input field

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
-import React from 'react';
+import React, {SyntheticEvent} from 'react';
 // import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 function reducer(notices: string) {
   let msg: string;
@@ -11,14 +12,30 @@ function reducer(notices: string) {
 
 export default function Home() {
   let [notices, dispatch] = React.useReducer(reducer, 'Ongoing Site Reconstruction');
+  let [inputVal, setInputVal] = React.useState('');
 
   const id = React.useId();
   const userId: string = `${id}-username`;
   const passId: string = `${id}-password`;
 
+  let router = useRouter();
   return(
     <>
       <div className="container min-w-full min-h-screen">
+        <form className='inline-flex w-screen place-content-center' onSubmit={(event: SyntheticEvent) => {
+          event.preventDefault();
+          if (event.target && /*(event.target as HTMLInputElement).value*/ inputVal === 'kx') router.push('https://blog.karnovah.com/');
+          setInputVal('');
+        }}>
+          <input 
+            className='relative z-0 text-center translate-y-[-10%] bg-black border-none rounded-none outline-none'
+            type="text"
+            value={inputVal}
+            onChange={event => {
+              setInputVal(event.target.value);
+            }}
+          />
+        </form>
         <div className="flex flex-auto bg-black min-h-screen">
           <div className="place-self-center mx-auto animate-bounce hover:animate-spin">
             <p className="z-10 font-sono text-[2rem] max-sm:text-[1.1rem]">{notices}</p>
