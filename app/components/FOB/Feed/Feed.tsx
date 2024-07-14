919b88af8b6c42bf591816b263017ee8d8160d7f
git <82054121+KXzeno@users.noreply.github.com>
2024-07-14 07:29:30 -0700

    Provision feeds as prop

--- app/components/FOB/Feed/Feed.tsx
+++ app/components/FOB/Feed/Feed.tsx
@@ -1,91 +1,44 @@
 import React from 'react';
-import Parser from 'rss-parser';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
 import 'dotenv/config';
 // import pg from 'pg';
-import { PrismaClient } from '@prisma/client';
-import { readReplicas } from '@prisma/extension-read-replicas';
 
-/* For manual ID fetch:
-   ytInitialData.contents.singleColumnBrowseResultsRenderer.tabs[0].tabRenderer.endpoint.browseEndpoint.browseId
- */
-
-let prisma = new PrismaClient().$extends(
-  readReplicas({
-    url: `${process.env.DATABASE_URL}`,
-  })
-);
-
-async function main() {
-  return await prisma.$replica().source.findMany();
-}
-
-async function getData() {
-  return main().then(async (res) => {
-    return res;
-  }).catch(async (e) => {
-    console.error(e);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
-}
-
-/*
-   let db = new pg.Client({
-user: 'avnadmin',
-password: process.env.DB_PASS_AIVEN,
-host: 'rss-feeds-kasz.e.aivencloud.com',
-port: 22108,
-database: 'defaultdb',
-ssl: {
-rejectUnauthorized: true,
-ca: process.env.DB_CA_CERT,
-},
-});
-
-db.connect();
-
-let fsrcs: any;
-
-db.query('SELECT * FROM Source', async (err, res) => {
-
-try {
-fsrcs = res.rows;
-// console.log(fsrcs, FEED_SRCS);
-} catch (e) {
-console.error('Query execution failed', e, err.stack);
-} finally {
-await db.end()
-}
-}); */
+import { readFeed, getData } from '@/actions';
 
 interface FeedParams {
-  filter?: string,
+  filter?: string;
+  src: Feed[];
 }
 
-export default async function Feed({ filter = undefined }: FeedParams): Promise<React.ReactElement> {
+interface Feed {
+  [key: number]: PostData
+}
+
+interface PostData {
+  title: string,
+  link: string,
+  description?: string,
+  pubDate?: string,
+  guid?: string,
+  contentSnippet?: string,
+  content: string,
+  author?: string,
+  id?: string,
+  ['content:encoded']?: string;
+}
+
+export default async function Feed({ filter = undefined, src }: FeedParams): Promise<React.ReactElement> {
   let fsrcs = await getData();
+  // let feeds = await readFeed();
+  let feeds = src;
+  let FEED_COUNT = feeds.length;
+
   if (filter !== undefined) {
     let validateFilter: boolean = !!(fsrcs.find(({ name }: { name: string }) => name.includes(filter)));
     if (!validateFilter || filter.length === 0) {
       notFound();
     }
-  }
-
-  let parser: Parser = new Parser();
-
-  let parse: (url: string) => Promise<object> = async url => {
-    let feed: { items: object } = await parser.parseURL(url);
-    if (!(feed && feed.items)) throw new Error('Invalid RSS URL');
-    // console.log(Object.getOwnPropertyNames(feed));
-    return feed.items as Array<object>;
-  }
-
-  let feeds: Array<Feed> = [];
-
-  interface Feed {
-    [key: number]: PostData
   }
 
   interface PostData {
@@ -101,43 +54,7 @@ export default async function Feed({ filter = undefined }: FeedParams): Promise<
     ['content:encoded']?: string;
   }
 
-  interface SourceModel {
-    id: number,
-    link: string,
-    name: string,
-  }
-
-  async function compilePosts(arr: Array<SourceModel>): Promise<Array<object>> {
-    if (filter !== undefined) {
-      arr = arr.filter(item => item.name === filter);
-    }
-
-    arr.filter(e => e.name === filter);
-    for (let i = 0; i < arr.length; i++) {
-      let post: object = await parse(arr[i].link);
-      feeds.push(post as Feed);
-    }
-    return [feeds[0]];
-  }
-
- // function spreadObjToArr(o: { [key: string]: string }): Array<string[]> {
- //   let newArray: Array<string[]> = [];
- //   Object.keys(o).forEach(key => {
- //     let dmarc: number = o[key].indexOf(',');
- //     let arrContent1: string = o[key].substring(0, dmarc);
- //     let arrContent2: string = o[key].substring(dmarc + 1);
- //     newArray.push([arrContent1, arrContent2]);
- //   });
- //   return newArray;
- // }
-
- // let srcs = spreadObjToArr(FEED_SRCS);
-  await compilePosts(fsrcs);
-  let FEED_COUNT: number = feeds.length;
-
-  //TODO: Fix truncation on long names
   function renderPosts(data: PostData) {
-    // console.log(data);
     if (data === undefined) {
       throw new Error('Incorrect Data Submitted.');
     }
