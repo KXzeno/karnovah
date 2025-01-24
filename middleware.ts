5f91783a854e46b2fe373e2e197c9076083fc1d1
unknown <82054121+KXzeno@users.noreply.github.com>
2025-01-23 21:45:54 -0800

    refactor: validate routes via switch

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,32 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export function middleware(request: NextRequest ) {
-  // Gets baseURL leaf and excludes '/'
-  let pathname = request.nextUrl.pathname.substring(1, request.nextUrl.pathname.length);
+	// Gets baseURL leaf and excludes '/'
+	let pathname = request.nextUrl.pathname.substring(1, request.nextUrl.pathname.length);
 
-  // Splits nested routes into names
-  let splitPaths = pathname.split('/');
+	// Splits nested routes into names
+	let splitPaths = pathname.split('/');
 
-  // Outputs nested routes to the new URL excluding '/blog' and '/blogs'
-  if (splitPaths[0].includes('blogs')) {
-    splitPaths.shift();
-    pathname = splitPaths.join('/');
-  } else if (splitPaths[0].includes('blog')) {
-    splitPaths.shift();
-    pathname = splitPaths.join('/');
-  }
+	let validRoutes: Array<string> = ["blog," "blogs," "about"];
 
-  return NextResponse.redirect(new URL(`https://blog.karnovah.com/${pathname}`, request.url));
+	switch (validRoutes.findIndex(splitPaths[0])) {
+		// Outputs nested routes to the new URL excluding '/blog' and '/blogs'
+		case 0: {
+			splitPaths.shift();
+			pathname = splitPaths.join('/');
+		} 
+		case 1: {
+			splitPaths.shift();
+			pathname = splitPaths.join('/');
+		}
+		case 2: {
+			return NextResponse.redirect(new URL(`https://github.com/KXzeno`, request.url));
+		}
+	}
+
+	return NextResponse.redirect(new URL(`https://blog.karnovah.com/${pathname}`, request.url));
 }
 
-export const config = {
-  matcher: ['/blog/:path*', '/blogs/:path*',],
-}
+	export const config = {
+		matcher: ['/blog/:path*', '/blogs/:path*',],
+	}
