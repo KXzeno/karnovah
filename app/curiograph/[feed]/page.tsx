3661cf6567df785ed6dbd6b387256731e163a6df
Kinz Sil <82054121+KXzeno@users.noreply.github.com>
2025-02-04 11:41:51 -0800

    FIX: rectify arg type
    - Expects a promise in params

--- app/curiograph/[feed]/page.tsx
+++ app/curiograph/[feed]/page.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import Feed from '@F/Feed';
 import { readFeed } from '@A/FeedActions';
 
-export default async function ExpandedFeed({ params }: { params: { feed: string }}): Promise<React.ReactNode> {
+export default async function ExpandedFeed({ params }: { params: Promise<{ feed: string }>}): Promise<React.ReactNode> {
   let feeds = await readFeed();
+  let feed = await params.then(res => res.feed);
   return (
     <>
-      <Feed src={feeds} filter={params.feed}/>
+      {<Feed src={feeds} filter={feed}/>}
     </>
   );
 }
